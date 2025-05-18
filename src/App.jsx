cdc46d0f0a64872fe63304df6c21d6a1a3f0e2ab
TCSon03 <truongcongsson@gmail.com>
2025-05-18 23:08:43 +0700

    done câu 1 câu 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import ProductList from './Components/ProductList'
+import React, { useState, useEffect } from "react";
+
+const url = "https://dummyjson.com/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(`${url}?page=1&limit=12`);
+      const data = await response.json();
+      setProducts(data.products);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div>
-      <ProductList />
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <h3>{product.title}</h3>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
