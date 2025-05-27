e1cdaeadfa6bda6ed919dbe536d97790f8177e51
TCSon03 <truongcongsson@gmail.com>
2025-05-27 20:09:32 +0700

    fix 07

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://react-code-farm-5os286ibz-tcson03s-projects.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
